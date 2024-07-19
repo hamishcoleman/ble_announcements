df85903a6bb179ef6998be8fb5bc8b2aa630338e
Hamish Coleman <hamish@zot.org>
2024-07-20 01:04:39 +1000

    Start decoding some of the internal data

--- lte_listen.pl
+++ lte_listen.pl
@@ -13,7 +13,140 @@ import argparse
 import bluetooth._bluetooth as bluez
 
 
+class MACAddr:
+    def __init__(self, buf):
+        self.addr = buf
+
+    def __str__(self):
+        a = []
+        for b in self.addr:
+            a.append(f"{b:02x}")
+        return ":".join(a)
+
+
+def _manuf(buf):
+    names = {
+        0x4c: "Apple",
+        0x75: "Samsung",
+        0x611: "Beurer",
+        0x6a8: "GD_Midea",
+    }
+    manuf = int.from_bytes(buf[0:2], byteorder="little")
+    if manuf in names:
+        manuf = names[manuf]
+    else:
+        manuf = f"0x{manuf:x}"
+    data = buf[2:]
+    return f"{manuf}:{data.hex()}"
+
+
+def _s8dBm(buf):
+    b = int.from_bytes(buf[0:1], byteorder="little", signed=True)
+    return f"{b}dBm"
+
+
+def _str(buf):
+    return buf.decode("utf8")
+
+
+def _u8hex(buf):
+    b = buf[0]
+    return f"0x{b:02x}"
+
+
+def _u16hex_nox(buf):
+    b = int.from_bytes(buf[0:2], byteorder="little")
+    return f"{b:04x}"
+
+
+class BLE_Tag:
+
+    tag_ids = {
+        0x01: {
+            "desc": "Flags",
+            "short": "F",
+            "str": _u8hex,
+        },
+        0x02: {
+            "desc": "UUID",
+            "short": "U",
+            "str": _u16hex_nox,
+        },
+        0x09: {
+            "desc": "Complete Local Name",
+            "short": "N",
+            "str": _str,
+        },
+        0x0a: {
+            "desc": "TX Power Level",
+            "short": "Tx",
+            "str": _s8dBm,
+        },
+        0x19: {
+            "desc": "Appearance",
+            "short": "Icon",
+            "str": _u16hex_nox,
+        },
+        0xff: {
+            "desc": "Manufacturer Specific",
+            "short": "M",
+            "str": _manuf,
+        },
+    }
+
+    def __init__(self, buf):
+        self.buf = buf
+
+    def __str__(self):
+        id = self.id()
+        if id in self.tag_ids:
+            tag_info = self.tag_ids[id]
+            return tag_info["short"] + ":" + tag_info["str"](self.data())
+        return str(self.buf)
+
+    def id(self):
+        return self.buf[0]
+
+    def data(self):
+        return self.buf[1:]
+
+
+class Message:
+    def __init__(self):
+        self.tags = []
+
+    def __str__(self):
+        s = []
+        s += [f"{self.addr}"]
+
+        if hasattr(self, "buf"):
+            s += [f"{self.buf}"]
+
+        for tag in self.tags:
+            s += [f"{tag}"]
+
+        return " ".join(s)
+
+    def add_tag(self, tag):
+        self.tags.append(tag)
+
+
 EVT_LE_META_EVENT = 0x3e
+
+
+def handle_buf_inner2(msg, buf):
+    """second layer wrapped message"""
+    # TODO: It probably has a type name
+    pos = 0
+
+    while pos < len(buf):
+        obj_len = buf[pos]
+        pos += 1
+        obj_buf = buf[pos:pos + obj_len]
+        pos += obj_len
+
+        tag = BLE_Tag(obj_buf)
+        msg.add_tag(tag)
 
 
 def handle_buf_inner1(buf):
@@ -21,20 +154,38 @@ def handle_buf_inner1(buf):
     # TODO: It probably has a type name
     pos = 0
 
+    msg = Message()
+
     # TODO:
-    # - this is a LTVi
+    # - this is a LTV
     # - how do we know it ends?
     flags = buf[pos:pos+3]
     pos += 3
-    if flags not in [b'\x02\x01\x00', b'\x02\x01\x03'] :
+    if flags not in [b'\x02\x01\x00', b'\x02\x01\x03']:
         raise ValueError(f"unexpected {flags} {pos} {buf}")
+    # TODO: msg.flags0 = flags
 
     # unknown
     if buf[pos] not in [0, 1]:
         raise ValueError(f"unexpected {buf[pos]} {pos} {buf}")
     pos += 1
+    # TODO: msg.?? =
 
-    print(buf)
+    # AdvA
+    msg.addr = MACAddr(buf[pos:pos+6][::-1])
+    pos += 6
+
+    len2 = buf[pos]
+    pos += 1
+
+    handle_buf_inner2(msg, buf[pos:pos + len2])
+    pos += len2
+
+    # TODO:
+    # always seems to be one more byte
+    # msg.?? = buf[pos]
+
+    return msg
 
 
 def handle_buf(buf):
@@ -106,7 +257,7 @@ def main():
 
     while True:
         buf = dev.recv(64)
-        handle_buf(buf)
+        print(handle_buf(buf))
 
     # If saved, restore
     # dev.setsockopt(bluez.SOL_HCI, bluez.HCI_FILTER, filter_saved)
