e4d788de1a905b668822098718fe2f3a4def7139
Hamish Coleman <hamish@zot.org>
2025-08-01 18:03:15 +0100

    Add decoding for one size of count measurements

--- bthome2influx.py
+++ bthome2influx.py
@@ -107,6 +107,11 @@ class BTHome:
                     "size": 1,
                     "type": "?",
                 },
+                0x3e: {
+                    "name": "count",
+                    "size": 4,
+                    "type": "<L",
+                },
             }
 
             if obj_id not in data_types:
